0b5fa54d32cec63aeee2732baff4ea8b46d71172
Yarom1R <93433709+Yarom1R@users.noreply.github.com>
2023-03-26 00:15:14 +0300

    Add files via upload

--- js/album.js
+++ js/album.js
@@ -45,19 +45,28 @@ function setUpAudio(){
         let progress = node.querySelector(`.progress`);
         
 
-        let isPlaying = false;
-        node.addEventListener(`click`, function () {
+        
+
+        node.addEventListener(`click`, function (evt) {
             
+            console.log(evt.target);
+            let progressBar = node.querySelector(`.progress`);
+            let progressBar1 = node.querySelector(`.progress-bar`);
+
             function updateProgress() {
                 if (audio.currentTime < audio.duration){
                     // Нарисовать актуальное время
                     timeNode.innerHTML = `${Math.floor(audio.currentTime/60/10)}${Math.floor(audio.currentTime/60)%10}:${Math.floor(audio.currentTime%60/10)}${Math.floor(audio.currentTime)%10}`;
 
                     // Нужно ли вызвать её ещё раз?
-                    if (isPlaying) {
+                    if (node.classList.contains(`isPlaying`)) {
                         requestAnimationFrame(updateProgress);
-                        node.querySelector(`.progress-bar`).style.width = `${100*audio.currentTime/audio.duration}%`
-                    }else{
+                        node.querySelector(`.progress-bar`).style.width = `${100*audio.currentTime/audio.duration}%`;
+                    }else if(node.classList.contains(`pause`)){
+                        console.log(`Трек на паузе`);
+                        trackIcon.src = `assets/pause.png`;
+                    }
+                    else{
                         timeNode.innerHTML = `${albums[j]['tracks'][i]['time']}`;
                         console.log(`ОНО ЗДЕСЬ БЫЛО`);
                         progress.classList.add(`d-none`);
@@ -69,7 +78,7 @@ function setUpAudio(){
                         timeNode.innerHTML = `${albums[j]['tracks'][i]['time']}`;
                         console.log(`ОНО ЗДЕСЬ БЫЛО`);
                         progress.classList.add(`d-none`);
-                        progress.classList.remove(`d-block`)
+                        progress.classList.remove(`d-block`);
                         trackIcon.src = `assets/4498521.png`;
                     }, 2000);
                     
@@ -77,29 +86,72 @@ function setUpAudio(){
                 
                 
               };
-            // Если трек сейчас играет...
-            if (!isPlaying && audio.currentTime <= audio.duration) {
-                isPlaying = true;
-                // Включить проигрывание
-                audio.play();
-                progress.classList.remove(`d-none`);
-                progress.classList.add(`d-block`)
-                
-                trackIcon.src = `assets/free-icon-music-album-4498611.png`;
-                updateProgress();
-                
-               
 
-            // Если трек сейчас не играет...
-            } else {
-                
+            if(evt.target != progressBar && evt.target != progressBar1){
+                if (!node.classList.contains(`isPlaying`) && !node.classList.contains(`pause`)) {
 
-                isPlaying = false;
+                    for (let i = 0; i < trackNodes.length; i++){
+                        trackNodes[i].querySelector(`.audio`).currentTime = 0;
+                        trackNodes[i].querySelector(`.audio`).pause();
+                        trackNodes[i].classList.remove(`isPlaying`);
+                        trackNodes[i].classList.remove(`pause`);
+                        trackNodes[i].querySelector(`.time`).innerHTML = `${albums[j]['tracks'][i]['time']}`;
+                        trackNodes[i].querySelector(`.progress`).classList.add(`d-none`);
+                        trackNodes[i].querySelector(`.progress`).classList.remove(`d-block`)
+                        trackNodes[i].querySelector(`.track-icon`).src = `assets/4498521.png`;
+                    }
+                    console.log(`ОНО ВКЛЮЧИЛОСЬ`);
+                    node.classList.add(`isPlaying`);
+                    // Включить проигрывание
+                    audio.play();
+                    progress.classList.remove(`d-none`);
+                    progress.classList.add(`d-block`);
+                    
+                    trackIcon.src = `assets/free-icon-music-album-4498611.png`;
+                    updateProgress();
+                    
+                   
+    
                 
-                
-                audio.pause();
-
-    }
+                } else if(!node.classList.contains(`isPlaying`) || node.classList.contains(`pause`)){
+                    for (let i = 0; i < trackNodes.length; i++){
+                        trackNodes[i].classList.remove(`isPlaying`);
+                        trackNodes[i].classList.remove(`pause`);
+                        trackNodes[i].querySelector(`.time`).innerHTML = `${albums[j]['tracks'][i]['time']}`;
+                        trackNodes[i].querySelector(`.progress`).classList.add(`d-none`);
+                        trackNodes[i].querySelector(`.progress`).classList.remove(`d-block`)
+                        trackNodes[i].querySelector(`.track-icon`).src = `assets/4498521.png`;
+                    }
+                    console.log(`ОНО ВКЛЮЧИЛОСЬ`);
+                    node.classList.add(`isPlaying`);
+                    // Включить проигрывание
+                    audio.play();
+                    progress.classList.remove(`d-none`);
+                    progress.classList.add(`d-block`);
+                    
+                    trackIcon.src = `assets/free-icon-music-album-4498611.png`;
+                    updateProgress();
+                }else {
+                    
+    
+                    node.classList.remove(`isPlaying`);
+                    node.classList.add(`pause`);
+                    
+                    
+                    audio.pause();
+    
+                }       
+            }else{
+                console.log(evt.clientX);
+                console.log(progressBar.getBoundingClientRect().x);
+                let w1 = progressBar.clientWidth;
+                let px = evt.clientX - progressBar.getBoundingClientRect().x;
+                let percent = 100*px/w1;
+                progressBar1.style.width = `${percent}%`
+                audio.currentTime = percent*audio.duration/100;
+                timeNode.innerHTML = `${Math.floor(audio.currentTime/60/10)}${Math.floor(audio.currentTime/60)%10}:${Math.floor(audio.currentTime%60/10)}${Math.floor(audio.currentTime)%10}`;
+            }
+            
 });
 
 
