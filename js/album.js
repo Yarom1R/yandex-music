f8269b03ac56bafe6b4b9b2a48f3b121297e8710
Yarom1R <93433709+Yarom1R@users.noreply.github.com>
2023-03-25 17:56:45 +0300

    Add files via upload

--- js/album.js
+++ js/album.js
@@ -27,13 +27,95 @@ let templateDescription = `
 `;
 
 
-let trackList = document.querySelector(`.track-list`);
-let description = document.querySelector(`.description`);
-let content = document.querySelector(`.content`);
-
 let search = new URLSearchParams(window.location.search);
 
 let j = search.get(`i`);
+
+function setUpAudio(){
+    let trackNodes = document.querySelectorAll(`.track`);
+
+    
+
+    for (let i = 0; i < trackNodes.length; i++) { 
+        console.log(i);
+        let node = trackNodes[i];   
+        let audio = node.querySelector(`.audio`); 
+        let trackIcon = node.querySelector(`.track-icon`);
+        let timeNode = node.querySelector(`.time`);
+        let progress = node.querySelector(`.progress`);
+        
+
+        let isPlaying = false;
+        node.addEventListener(`click`, function () {
+            
+            function updateProgress() {
+                if (audio.currentTime < audio.duration){
+                    // Нарисовать актуальное время
+                    timeNode.innerHTML = `${Math.floor(audio.currentTime/60/10)}${Math.floor(audio.currentTime/60)%10}:${Math.floor(audio.currentTime%60/10)}${Math.floor(audio.currentTime)%10}`;
+
+                    // Нужно ли вызвать её ещё раз?
+                    if (isPlaying) {
+                        requestAnimationFrame(updateProgress);
+                        progress.innerHTML = `
+                            <div class="progress-bar progress-bar-striped" role="progressbar" style = "height: 16px; width: ${100*audio.currentTime/audio.duration}%" aria-label="Basic example" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+                        `
+                    }else{
+                        timeNode.innerHTML = `${albums[j]['tracks'][i]['time']}`;
+                        console.log(`ОНО ЗДЕСЬ БЫЛО`);
+                        progress.classList.add(`d-none`);
+                        progress.classList.remove(`d-block`)
+                        trackIcon.src = `assets/4498521.png`;
+                    }
+                }else{
+                    setTimeout(() => {
+                        timeNode.innerHTML = `${albums[j]['tracks'][i]['time']}`;
+                        console.log(`ОНО ЗДЕСЬ БЫЛО`);
+                        progress.classList.add(`d-none`);
+                        progress.classList.remove(`d-block`)
+                        trackIcon.src = `assets/4498521.png`;
+                    }, 2000);
+                    
+            }
+                
+                
+              };
+            // Если трек сейчас играет...
+            if (!isPlaying && audio.currentTime <= audio.duration) {
+                isPlaying = true;
+                // Включить проигрывание
+                audio.play();
+                progress.classList.remove(`d-none`);
+                progress.classList.add(`d-block`)
+                
+                trackIcon.src = `assets/free-icon-music-album-4498611.png`;
+                updateProgress();
+                
+               
+
+            // Если трек сейчас не играет...
+            } else {
+                
+
+                isPlaying = false;
+                
+                
+                audio.pause();
+
+    }
+});
+
+
+        
+    }
+};
+
+
+
+let trackList = document.querySelector(`.track-list`);
+let description = document.querySelector(`.description`);
+let musicList = document.querySelector(`.music-list`);
+
+
 
 description.innerHTML = `
     <div class="card mb-3" style="max-width: 540px;">
@@ -54,14 +136,28 @@ description.innerHTML = `
 
 for (let i = 0; i < albums[j]['tracks'].length; i++){
     trackList.innerHTML += `
-        <li class="list-group-item d-flex justify-content-start align-items-center track">
+        <li class="list-group-item  track">
+            <div class = "d-flex justify-content-start align-items-center">
             <img src="assets/4498521.png" alt="" class = "track-icon">
             <div class="ms-2 me-auto">
                 <div class="fw-bold">${albums[j]['tracks'][i]['trackTitle']}</div>
                 ${albums[j]['tracks'][i]['autor']}
             </div>
             <p class = "time">${albums[j]['tracks'][i]['time']}</p>
+            <audio class="audio" src="${albums[j]['tracks'][i]['src']}"></audio>
+            </div>
+            <div class="progress d-none">
+                
+            </div>
         </li>
-    `
+        
+        
+    `;
 };
+
+
+setUpAudio();
+
+
+
 
--- js/data.js
+++ js/data.js
@@ -8,22 +8,26 @@ albums = [
             track1 = {
                 trackTitle: `Experience`,
                 autor: `Ludovico Einaudi, Daniel Hope, I Virtuosi Italiani`,
-                time: `5:12`
+                time: `5:15`,
+                src: `music/Experience - ludovico.mp3`
             },
             track2 = {
                 trackTitle: `Comptine d'un autre été, l'après-midi`,
                 autor: `Martin Jacoby`,
-                time: `2:02`
+                time: `2:02`,
+                src: `music/Martin_Jacoby.mp3`
             },
             track3 = {
                 trackTitle: `Jam Jah Dub`,
                 autor: `Shambala`,
-                time: `6:20`
+                time: `6:20`,
+                src: `music/Shambala-JamJahDub.mp3`
             },
             track4 = {
                 trackTitle: `Dead Already`,
                 autor: `Thomas Newman`,
-                time: `3:18`
+                time: `3:18`,
+                src: `music/Thomas Newman — Dead Already.mp3`
             },
         ]
     },
@@ -36,22 +40,26 @@ albums = [
             track1 = {
                 trackTitle: `Therefore I Am`,
                 autor: `Billie Eilish`,
-                time: `2:54`
+                time: `2:54`,
+                src: `music/Billie Eilish - Therefore I Am.mp3`
             },
             track2 = {
                 trackTitle: `Levitating`,
                 autor: `Dua Lipa`,
-                time: `3:23`
+                time: `3:23`,
+                src: `music/Dua Lipa feat. DaBaby - Levitating.mp3`
             },
             track3 = {
                 trackTitle: `Take You Dancing`,
                 autor: `Jason Derulo`,
-                time: `3:10`
+                time: `3:10`,
+                src: `music/Jason Derulo - Take You Dancing.mp3`
             },
             track4 = {
                 trackTitle: `dramatic`,
                 autor: `Cat & Calmell`,
-                time: `2:30`
+                time: `2:30`,
+                src: `music/Cat & Calmell - Dramatic.mp3`
             },
         ]
     },
@@ -64,22 +72,26 @@ albums = [
             track1 = {
                 trackTitle: `La Solitudine`,
                 autor: `Orquesta Lírica de Barcelona`,
-                time: `3:59`
+                time: `3:59`,
+                src: `music/Orquesta Lírica de Barcelona - La Solitudine.mp3`
             },
             track2 = {
                 trackTitle: `Havana`,
                 autor: `Kenny G`,
-                time: `5:29`
+                time: `5:23`,
+                src: `music/Kenny G — Havana.mp3`
             },
             track3 = {
                 trackTitle: `I Believe`,
                 autor: `Dave Koz`,
-                time: `5:31`
+                time: `5:31`,
+                src: `music/Dave Koz - I Believe.mp3`
             },
             track4 = {
                 trackTitle: `Fallen`,
                 autor: `Warren Hill`,
-                time: `4:17`
+                time: `4:17`,
+                src: `music/Warren Hill — Fallen.mp3`
             },
         ]
     },
@@ -92,22 +104,26 @@ albums = [
             track1 = {
                 trackTitle: `Where Have You Been`,
                 autor: `Rihanna`,
-                time: `4:02`
+                time: `4:02`,
+                src: `music/Rihanna - Where Have You Been.mp3`
             },
             track2 = {
                 trackTitle: `In Your Eyes`,
                 autor: `Robin Schulz, Alida`,
-                time: `3:28`
+                time: `3:27`,
+                src: `music/Robin Schulz — In Your Eyes (feat. Alida).mp3`
             },
             track3 = {
                 trackTitle: `Something Just Like This`,
                 autor: `Coldplay, The Chainsmokers`,
-                time: `4:32`
+                time: `4:07`,
+                src: `music/The Chainsmokers & Coldplay - Something Just Like This.mp3`
             },
             track4 = {
                 trackTitle: `Gold`,
                 autor: `Syn Cole, Graham Candy`,
-                time: `2:54`
+                time: `2:54`,
+                src: `music/Syn Cole - Gold (feat. Graham Candy).mp3`
             },
         ]
     }
